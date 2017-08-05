4091f6330d3b941a11fd7d66275b6f51a5b63ac6
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-08-05 11:12:47 +0000

    Combined custom user fields into single migration file.

---
+++ db/migrate/20170728181451_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :username, :string
+    add_column :users, :bio, :text
+  end
+end
