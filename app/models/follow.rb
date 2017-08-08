acad5b2cf885d1f34279479b1cf29153c228eadc
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-08-08 09:37:58 +0000

    Fix uninitialized constant error.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -9,7 +9,7 @@ class Follow < ActiveRecord::Base
     def send_notification_to_followee
       follower = User.find_by_id(self.follower_id)
       followee =  User.find_by_id(self.followee_id)
-      fcm = FCM.new(ENV[FCM_KEY])
+      fcm = FCM.new(ENV["FCM_KEY"])
       registration_ids = Array.wrap(followee.fcm_token)
       options = {data: {message: "\"#{follower.name} #{follower.surname} is following you.\""}}
       fcm.send(registration_ids, options)
